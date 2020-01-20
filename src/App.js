ce1168688c28b0000ec003222dc24cb6790c9548
meaghanbass <meaghan.bass@gmail.com>
2020-01-20 13:13:04 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './images/logo.svg';
+import './App.scss';
+import ballerina from './images/ballerina.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App"></div>
+
+      <div className="wrapper">
+        <header className="App-header">
+          <div className="left page-title">
+            <h1>The History<br />of Sculpture</h1>
+          </div>
+          <div className="middle center-image">
+            <img src={ballerina} alt="ballerina" />
+          </div>
+          <div className="right menu">
+            <ul>
+              <li>one</li>
+              <li>two</li>
+              <li>three</li>
+            </ul>
+          </div>
+        </header>
+      </div>
+    </>
   );
 }
 
